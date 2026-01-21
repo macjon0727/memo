2f5c75e9a33334e22e5086031da99e3e85511afc
kangminjun <coja0727@gmail.com>
2026-01-21 11:53:19 +0900

    refactor: JPA가 스캔 할 수 있는 엔터티 클래스로 변경(ORM)

--- src/main/java/com/meta/memo/domain/Memo.java
+++ src/main/java/com/meta/memo/domain/Memo.java
@@ -1,6 +1,7 @@
 package com.meta.memo.domain;
 
 import com.meta.memo.dto.MemoRequestDto;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,9 +9,17 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name="memo")
+@Entity
 public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //계정 아니고 인덱스임
+
+    @Column(name = "Username", nullable = false)
     private String username;
+
+    @Column(name = "contents", nullable = false, length = 500)
     private String contents;
 
     public Memo(MemoRequestDto memoRequestDto) {
